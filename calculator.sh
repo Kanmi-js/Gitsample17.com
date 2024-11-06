562fdebca556e48227e9e738a304c53f88e45179
Ubuntu <ubuntu@ip-172-31-42-153.eu-north-1.compute.internal>
2024-11-06 10:39:27 +0000

    this is my second version

--- calculator.sh
+++ calculator.sh
@@ -1,11 +1,28 @@
-x = a+ b
-
-if a - b = i
-       i = range[ 1..5}
-	echo "x is an even number"
-else a + b = {i > 5}
-	echo " x is a prime number"
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        prev = dummy
+        current = head
+        
+        while current:
+            has_duplicates = False
+            
+            while current.next and current.val == current.next.val:
+                current = current.next
+                has_duplicates = True
+            
+            if has_duplicates:
+                prev.next = current.next
+            else:
+                prev = prev.next
+        
+            current = current.next
+        return dummy.next
 fi
-done
-	
-	
